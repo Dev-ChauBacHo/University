a4d6a9f8991a7ea26f58c9130c75d46235b04468
TranVinh <vinhmone99@gmail.com>
2020-10-07 10:31:55 +0700

    Revert "Binary Numbers"
    This reverts commit 4eea70d7

---
+++ src/PTIT_Practice/HeThongSoHoa.java
@@ -0,0 +1,16 @@
+package PTIT_Practice;
+
+import java.util.Scanner;
+
+public class HeThongSoHoa {
+    private static final Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+        String s;
+        System.out.println(Math.sqrt(99999999999d));
+//        for (int i = 1; i <= 10; i++) {
+//            s = reader.nextLine();
+//            s = s.replaceAll("[,]\\s", " - ");
+//            System.out.println(s);
+//        }
+    }
+}
